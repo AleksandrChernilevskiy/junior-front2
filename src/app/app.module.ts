e2fd9f4548cbd0dfa308ed46823a917be2dcebd5
Александр Чернилевский <aleksanderterar@gmail.com>
2021-12-07 03:15:04 +0300

    zadanie2.2

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TabsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/tabs/tabs.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+// import { HttpClient } from '@angular/common/http'; 
+
+// import data from './data.json';
+
+//   const urls = [
+// {
+//   url: 'data.json',
+//   json: data,
+// }
+// ]
+
+
+@Component({
+  selector: 'tab-group-dynamic-height-example',
+  templateUrl: 'tabs.component.html',
+  styleUrls: ['tabs.component.css'],
+})
+export class TabsComponent implements OnInit {
+
+  // getInfo () {
+  //   return data;
+  // }
+
+  // function = [Object.entries(data)];
+
+  ngOnInit() {
+  //   // let filtredArr = data.data.filter( function( k ){ 
+  //   // k => k.type == "outcome" })
+  //   let newData = [];
+  //   newData = Object.entries(data);
+  //   // console.log(newData);
+  //   // console.log(data);
+  //   // return console.log(data.data[0].name, data.data[1].name);
+  //   // return console.log(data);
+  //   // console.log(data.Data.filter( item => item.type ));
+  }
+}
